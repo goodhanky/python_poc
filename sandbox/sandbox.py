c46c38e6463674e2b3c07c7a5bebda577a95d595
goodhanky <goodhanky@protonmail.com>
2022-07-09 08:13:23 -0700

    adding sandbox for python

---
+++ sandbox/sandbox.py
@@ -0,0 +1,22 @@
+###################################
+# Fundamentals data types - Numbers
+###################################
+
+# print(type(5 + 5))
+# print(type(5 - 5))
+
+# print(2 ** 5)
+# print(type(2 // 5))
+
+# print(round(5.6))
+
+
+########
+# String
+########
+
+# firstname = input('what is your first name? ')
+# lastname = input('what is your last name?')
+
+# print('Hello' + ' ' + firstname + ' ' + lastname)
+
